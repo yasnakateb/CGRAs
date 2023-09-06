6fa82295af51d0fda4837fccca6419e1524058d0
yasnakateb <yasnakatebzadeh@gmail.com>
2023-09-06 11:50:46 +0200

    Remove the duplication of the first data, Todo: Fix dout_v

--- src/main/scala/D_FIFO.scala
+++ src/main/scala/D_FIFO.scala
@@ -58,30 +58,58 @@ class D_FIFO
     val read_pointer = RegInit(0.U(FIFO_DEPTH.W)) 
     val num_data = RegInit(0.U(FIFO_DEPTH.W)) 
 
+    /*
     val full = RegInit(0.U)  	
     val empty = RegInit(0.U) 
     val rd_en = RegInit(0.U)  	
     val wr_en = RegInit(0.U)  	
+    */
+
+    val full = Wire(Bool()) 
+    val empty = Wire(Bool()) 
+    val rd_en = Wire(Bool()) 
+    val wr_en = Wire(Bool()) 
 
     val dout_v = RegInit(0.U) 
-    val dout = RegInit(0.U(DATA_WIDTH.W)) 
+    //val dout = RegInit(0.U(DATA_WIDTH.W)) 
 
     empty := true.B
     full := false.B 
-    dout := 0.U 
-    dout_v := false.B  
+    //dout := 0.U 
+    //dout_v := false.B  
+
+    io.dout := 0.U 
+    io.dout_v := false.B 
 
     wr_en := io.din_v & (~full) 
     rd_en := io.dout_r & (~empty) 
 
     when (io.dout_r) {
-        dout_v := false.B  
+        io.dout_v := false.B  
+    }
+
+    when (empty === false.B  &  rd_en === true.B & full === false.B  &  wr_en === true.B){
+        num_data := num_data
+        io.dout := io.din
+    }.elsewhen(full === false.B  &  wr_en === true.B){
+        num_data := num_data + 1.U
+        memory(write_pointer) := io.din
+
+    }.elsewhen(empty === false.B  &  rd_en === true.B){
+        when(num_data =/= 0.U){
+            num_data := num_data - 1.U 
+            io.dout := memory(read_pointer)
+        }.otherwise{
+            num_data := num_data
+        }
+    }.otherwise{
+        num_data := num_data
     }
 
     when(full === false.B  &  wr_en === true.B){ 
-        memory(write_pointer) := io.din;
-        num_data := num_data + 1.U
-
+        // memory(write_pointer) := io.din
+        //num_data := num_data + 1.U
+        
         when (write_pointer === FIFO_DEPTH.U - 1.U)  {
             write_pointer := 0.U 
         }.otherwise {
@@ -91,17 +119,17 @@ class D_FIFO
 
     // FIX (Read and write at the same time)
     when(empty === false.B  &  rd_en === true.B){ 
-        dout := memory(read_pointer)
-        dout_v := true.B 
-        num_data := num_data - 1.U 
-        
+        // io.dout := memory(read_pointer)
+        when (num_data =/= 0.U){
+            io.dout_v := true.B 
+        }
         when (read_pointer === FIFO_DEPTH.U - 1.U)  {
             read_pointer := 0.U 
         }.otherwise {
             read_pointer := read_pointer + 1.U  
         }
     } 
-
+    
     when (num_data === FIFO_DEPTH.U)  {
         full := true.B 
     }.otherwise {
@@ -115,8 +143,8 @@ class D_FIFO
     }  
 
     io.din_r := (~full)  
-    io.dout_v := dout_v  
-    io.dout := dout                    
+    //io.dout_v := dout_v  
+    //io.dout := dout                    
 }
 
 // Generate the Verilog code
