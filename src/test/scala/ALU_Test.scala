991e9a2f18abba95650485b1bfb4d981ff507af3
yasnakateb <yasnakatebzadeh@gmail.com>
2023-09-07 11:45:28 +0200

    Add more tests for ALU

--- src/test/scala/ALU_Test.scala
+++ src/test/scala/ALU_Test.scala
@@ -38,11 +38,10 @@ import org.scalatest.flatspec.AnyFlatSpec
 class ALU_Test extends AnyFlatSpec with ChiselScalatestTester {
     "ALU_Test test" should "pass" in {
         test(new ALU(32, 4)) { dut =>
-            var din_1 = 8.U
-            var din_2 = 3.U
+
+            dut.io.din_1.poke(1.U)
+            dut.io.din_2.poke(2.U)
             var op_config = 0.U
-            dut.io.din_1.poke(din_1)
-            dut.io.din_2.poke(din_2)
             ////////////////////////////////////////////////////////////////
             // Test 1 
             ////////////////////////////////////////////////////////////////
@@ -52,63 +51,148 @@ class ALU_Test extends AnyFlatSpec with ChiselScalatestTester {
             println("Din1: " + dut.io.din_1.peek().toString)
             println("Din2: " + dut.io.din_2.peek().toString)
             // Summation  
-            dut.io.op_config.poke(op_config)
+            dut.clock.step(1)
+            dut.io.din_1.poke(3.U)
+            dut.io.din_2.poke(4.U)
+            dut.clock.step(1)
+            dut.io.din_1.poke(5.U)
+            dut.io.din_2.poke(6.U)
             dut.clock.step(1)
             println("Summation: " + dut.io.dout.peek().toString)
             // Multiplication
+            dut.io.din_1.poke(2.U)
+            dut.io.din_2.poke(1.U)
             op_config = 1.U
             dut.io.op_config.poke(op_config)
+            dut.clock.step(1)
+            dut.io.din_1.poke(4.U)
+            dut.io.din_2.poke(3.U)
+            dut.clock.step(1)
+            dut.io.din_1.poke(6.U)
+            dut.io.din_2.poke(5.U)
             dut.clock.step(1)
             println("Multiplication: " + dut.io.dout.peek().toString)
             // Subtraction
             op_config = 2.U
+            dut.io.din_1.poke(2.U)
+            dut.io.din_2.poke(0.U)
             dut.io.op_config.poke(op_config)
+            dut.clock.step(1)
+            dut.io.din_1.poke(4.U)
+            dut.io.din_2.poke(1.U)
+            dut.clock.step(1)
+            dut.io.din_1.poke(6.U)
+            dut.io.din_2.poke(4.U)
             dut.clock.step(1)
             println("Subtraction: " + dut.io.dout.peek().toString)
             // Shift Left Logical
             op_config = 3.U
+            dut.io.din_1.poke(2.U)
+            dut.io.din_2.poke(1.U)
             dut.io.op_config.poke(op_config)
             dut.clock.step(1)
-            println("Left Logical: " + dut.io.dout.peek().toString)
+            dut.io.din_1.poke(4.U)
+            dut.io.din_2.poke(3.U)
+            dut.clock.step(1)
+            dut.io.din_1.poke(6.U)
+            dut.io.din_2.poke(5.U)
+            dut.clock.step(1)
+            println("Shift Left Logical: " + dut.io.dout.peek().toString)
             // Shift Right Arithmetic
             op_config = 4.U
+            dut.io.din_1.poke(20.U)
+            dut.io.din_2.poke(1.U)
             dut.io.op_config.poke(op_config)
+            dut.clock.step(1)
+            dut.io.din_1.poke(14.U)
+            dut.io.din_2.poke(1.U)
+            dut.clock.step(1)
+            dut.io.din_1.poke(16.U)
+            dut.io.din_2.poke(2.U)
             dut.clock.step(1)
             println("Shift Right Arithmetic: " + dut.io.dout.peek().toString)
             // Shift Right Logical
             op_config = 5.U
+            dut.io.din_1.poke(20.U)
+            dut.io.din_2.poke(1.U)
             dut.io.op_config.poke(op_config)
+            dut.clock.step(1)
+            dut.io.din_1.poke(14.U)
+            dut.io.din_2.poke(1.U)
+            dut.clock.step(1)
+            dut.io.din_1.poke(16.U)
+            dut.io.din_2.poke(2.U)
             dut.clock.step(1)
             println("Shift Right Logical: " + dut.io.dout.peek().toString)
             // And
             op_config = 6.U
+            dut.io.din_1.poke(20.U)
+            dut.io.din_2.poke(16.U)
             dut.io.op_config.poke(op_config)
+            dut.clock.step(1)
+            dut.io.din_1.poke(14.U)
+            dut.io.din_2.poke(13.U)
+            dut.clock.step(1)
+            dut.io.din_1.poke(17.U)
+            dut.io.din_2.poke(15.U)
             dut.clock.step(1)
             println("And: " + dut.io.dout.peek().toString)
             // Or
             op_config = 7.U
+            dut.io.din_1.poke(20.U)
+            dut.io.din_2.poke(1.U)
             dut.io.op_config.poke(op_config)
+            dut.clock.step(1)
+            dut.io.din_1.poke(14.U)
+            dut.io.din_2.poke(2.U)
+            dut.clock.step(1)
+            dut.io.din_1.poke(16.U)
+            dut.io.din_2.poke(3.U)
             dut.clock.step(1)
             println("Or: " + dut.io.dout.peek().toString)
             // Xor
             op_config = 8.U
+            dut.io.din_1.poke(20.U)
+            dut.io.din_2.poke(1.U)
             dut.io.op_config.poke(op_config)
+            dut.clock.step(1)
+            dut.io.din_1.poke(14.U)
+            dut.io.din_2.poke(2.U)
+            dut.clock.step(1)
+            dut.io.din_1.poke(16.U)
+            dut.io.din_2.poke(3.U)
             dut.clock.step(1)
             println("Xor: " + dut.io.dout.peek().toString)
             // Minimum
             op_config = 10.U
+            dut.io.din_1.poke(20.U)
+            dut.io.din_2.poke(1.U)
             dut.io.op_config.poke(op_config)
+            dut.clock.step(1)
+            dut.io.din_1.poke(14.U)
+            dut.io.din_2.poke(2.U)
+            dut.clock.step(1)
+            dut.io.din_1.poke(16.U)
+            dut.io.din_2.poke(3.U)
             dut.clock.step(1)
             println("Minimum: " + dut.io.dout.peek().toString)
             // Maximum
             op_config = 11.U
+            dut.io.din_1.poke(20.U)
+            dut.io.din_2.poke(1.U)
             dut.io.op_config.poke(op_config)
+            dut.clock.step(1)
+            dut.io.din_1.poke(14.U)
+            dut.io.din_2.poke(2.U)
+            dut.clock.step(1)
+            dut.io.din_1.poke(16.U)
+            dut.io.din_2.poke(3.U)
             dut.clock.step(1)
             println("Maximum: " + dut.io.dout.peek().toString)
             ////////////////////////////////////////////////////////////////
             // Test 2
             ////////////////////////////////////////////////////////////////
-            din_1 = "b1000_0000_0000_0000_0000_0000_0000_1111".U
+            val din_1 = "b1000_0000_0000_0000_0000_0000_0000_1111".U
             dut.io.din_1.poke(din_1)
             println("*************************************")
             println("Test 2: Din1 => Negative number")
