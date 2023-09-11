8c95819bf03be9207ba8772d210e41f12e13abb6
yasnakateb <yasnakatebzadeh@gmail.com>
2023-09-11 11:13:31 +0200

    Original d-fifo: rd_en and wr_en => wire, others => reg, without any conditios for num_data

--- src/main/scala/D_FIFO.scala
+++ src/main/scala/D_FIFO.scala
@@ -52,23 +52,16 @@ class D_FIFO
         val dout_v = Output(Bool()) 
     })
 
-    val memory = SyncReadMem(FIFO_DEPTH, UInt(DATA_WIDTH.W))
+    val memory = Mem(FIFO_DEPTH, UInt(DATA_WIDTH.W))
 
     val write_pointer = RegInit(0.U(FIFO_DEPTH.W)) 
     val read_pointer = RegInit(0.U(FIFO_DEPTH.W)) 
     val num_data = RegInit(0.U(FIFO_DEPTH.W)) 
 
-    /*
-    val full = RegInit(0.U)  	
+    val full = RegInit(0.U) 
     val empty = RegInit(0.U) 
-    val rd_en = RegInit(0.U)  	
-    val wr_en = RegInit(0.U)  	
-    */
-
-    val full = Wire(Bool()) 
-    val empty = Wire(Bool()) 
-    val rd_en = Wire(Bool()) 
-    val wr_en = Wire(Bool()) 
+    val rd_en = Wire(Bool())
+    val wr_en = Wire(Bool())
 
     val dout_v = RegInit(0.U) 
     val dout = RegInit(0.U(DATA_WIDTH.W)) 
@@ -78,39 +71,14 @@ class D_FIFO
     dout := 0.U 
     dout_v := false.B  
 
-    //io.dout := 0.U 
-    //io.dout_v := false.B 
-
-    wr_en := io.din_v & (~full) 
-    rd_en := io.dout_r & (~empty) 
-
     when (io.dout_r) {
-        dout_v := false.B  
-    }
-
-    when ( (empty === false.B  &  rd_en === true.B) & (full === false.B  &  wr_en === true.B)){
-        num_data := num_data
-        dout := io.din
-    }.elsewhen(full === false.B  &  wr_en === true.B){
-        num_data := num_data + 1.U
-        memory(write_pointer) := io.din
-
-    }.elsewhen(empty === false.B  &  rd_en === true.B){
-        when(num_data =/= 0.U){
-            num_data := num_data - 1.U 
-            dout := memory(read_pointer)
-            
-        }.otherwise{
-            num_data := num_data
-        }
-    }.otherwise{
-        num_data := num_data
+        io.dout_v := false.B  
     }
 
     when(full === false.B  &  wr_en === true.B){ 
-        // memory(write_pointer) := io.din
-        //num_data := num_data + 1.U
-        
+        memory(write_pointer) := io.din;
+        num_data := num_data + 1.U
+
         when (write_pointer === FIFO_DEPTH.U - 1.U)  {
             write_pointer := 0.U 
         }.otherwise {
@@ -120,17 +88,17 @@ class D_FIFO
 
     // FIX (Read and write at the same time)
     when(empty === false.B  &  rd_en === true.B){ 
-        // io.dout := memory(read_pointer)
-        when (num_data =/= 0.U){
-            dout_v := true.B 
-        }
+        dout := memory(read_pointer)
+        dout_v := true.B 
+        num_data := num_data - 1.U 
+        
         when (read_pointer === FIFO_DEPTH.U - 1.U)  {
             read_pointer := 0.U 
         }.otherwise {
             read_pointer := read_pointer + 1.U  
         }
     } 
-    
+
     when (num_data === FIFO_DEPTH.U)  {
         full := true.B 
     }.otherwise {
@@ -143,7 +111,10 @@ class D_FIFO
         empty := false.B
     }  
 
-    io.din_r := (~full)  
+    wr_en := io.din_v & (~full) 
+    rd_en := io.dout_r & (~empty) 
+    
+    io.din_r := (~full) 
     io.dout_v := dout_v  
     io.dout := dout                    
 }
