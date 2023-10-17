e44c38132bb66a1c8922c9f07530c24a6aaacd0a
yasnakateb <yasnakatebzadeh@gmail.com>
2023-10-17 14:51:44 +0200

    Fix D-FIFO

--- src/main/scala/D_FIFO.scala
+++ src/main/scala/D_FIFO.scala
@@ -34,7 +34,7 @@
 import chisel3._
 import chisel3.util._
 
-class D_FIFO 
+class D_FIFO
     (
         DATA_WIDTH: Int, 
         FIFO_DEPTH: Int
@@ -52,73 +52,69 @@ class D_FIFO
         val dout_v = Output(Bool()) 
     })
 
-    val memory = Mem(FIFO_DEPTH, UInt(DATA_WIDTH.W))
+    val cntWrite = RegInit(0.U(log2Ceil(FIFO_DEPTH).W))
+    val cntRead = RegInit(0.U(log2Ceil(FIFO_DEPTH).W))
+    val cntData = RegInit(0.U(log2Ceil(FIFO_DEPTH + 1).W))
 
-    val write_pointer = RegInit(0.U(FIFO_DEPTH.W)) 
-    val read_pointer = RegInit(0.U(FIFO_DEPTH.W)) 
-    val num_data = RegInit(0.U(FIFO_DEPTH.W)) 
+    val mem = SyncReadMem(FIFO_DEPTH, UInt(DATA_WIDTH.W))
 
-    val full = RegInit(0.U) 
-    val empty = RegInit(0.U) 
-    val rd_en = Wire(Bool())
-    val wr_en = Wire(Bool())
 
-    val dout_v = RegInit(0.U) 
-    val dout = RegInit(0.U(DATA_WIDTH.W)) 
+    val wen = Wire(Bool())
+    val ren = Wire(Bool())
+    val empty = Wire(Bool())
+    val full = Wire(Bool())
+    val dout_v = RegNext(0.U) 
 
-    empty := true.B
-    full := false.B 
-    dout := 0.U 
-    dout_v := false.B 
+    io.din_r := ~full 
+    wen := io.din_v & ~full
+    ren := io.dout_r & ~empty
 
-    wr_en := io.din_v & (~full) 
-    rd_en := io.dout_r & (~empty) 
+    dout_v := ren
     
-    io.din_r := (~full) 
-    
-
-    when (io.dout_r) {
-        dout_v := false.B  
+    io.dout_v := dout_v 
+    // Write pointer
+    when(wen & ~full) {
+        when(cntWrite === FIFO_DEPTH.U - 1.U) {
+        cntWrite := 0.U
+        }.otherwise {
+        cntWrite := cntWrite + 1.U
+        }
     }
 
-    when(full === false.B  &  wr_en === true.B){ 
-        memory(write_pointer) := io.din;
-        num_data := num_data + 1.U
-
-        when (write_pointer === FIFO_DEPTH.U - 1.U)  {
-            write_pointer := 0.U 
+    // Read pointer
+    when(ren & ~empty) {
+        when(cntRead === FIFO_DEPTH.U - 1.U) {
+            cntRead := 0.U
         }.otherwise {
-            write_pointer := write_pointer + 1.U 
+            cntRead := cntRead + 1.U
         }
-    }            
+        }
 
-    // FIX (Read and write at the same time)
-    when(empty === false.B  &  rd_en === true.B){ 
-        dout := memory(read_pointer)
-        dout_v := true.B 
-        num_data := num_data - 1.U 
-        
-        when (read_pointer === FIFO_DEPTH.U - 1.U)  {
-            read_pointer := 0.U 
+    // Data counter
+    when((wen & ~ full) & ~(ren & ~empty)) {
+        cntData := cntData + 1.U
+        }.elsewhen(~(wen & ~full) & (ren & ~empty)) {
+        cntData := cntData - 1.U
+    }
+
+    // Memory access
+    when(wen & ~full) {
+        mem.write(cntWrite, io.din)
+        }
+    io.dout := mem.read(cntRead)
+
+    // Control signal generation
+    when(cntData === FIFO_DEPTH.U) {
+        full := true.B
         }.otherwise {
-            read_pointer := read_pointer + 1.U  
-        }
-    } 
-
-    when (num_data === FIFO_DEPTH.U)  {
-        full := true.B 
-    }.otherwise {
-        full := false.B 
-    }           
-
-    when (num_data === 0.U)  {
-        empty := true.B 
-    }.otherwise {
+        full := false.B
+    }
+    when(cntData === 0.U) {
+        empty := true.B
+        }.otherwise {
         empty := false.B
-    }   
+    }
 
-    io.dout_v := dout_v  
-    io.dout := dout                    
 }
 
 // Generate the Verilog code
--- src/test/scala/D_FIFO_Test.scala
+++ src/test/scala/D_FIFO_Test.scala
@@ -44,419 +44,75 @@ class D_FIFO_Test extends AnyFlatSpec with ChiselScalatestTester {
             
             dut.io.dout_r.poke(dout_r)
             dut.clock.step(1)
-            dut.io.din.poke(66.U)
+            dut.io.din.poke(1.U)
             dut.io.din_v.poke(din_v)
             dut.clock.step(1)
-            dut.io.din.poke(3.U)
-            dut.clock.step(1)
-            dut.io.din.poke(5.U)
-            dut.clock.step(1)
-            dut.io.din.poke(7.U)
-            dut.clock.step(1)
-            dut.io.din.poke(9.U)
+            dut.io.din.poke(2.U)
             dut.clock.step(1)
             dut.io.din.poke(3.U)
             dut.clock.step(1)
+            dut.io.din.poke(4.U)
+            dut.clock.step(1)
             dut.io.din.poke(5.U)
+            dut.clock.step(1)
+            dut.io.din.poke(6.U)
             dut.clock.step(1)
             dut.io.din.poke(7.U)
             dut.clock.step(1)
+            dut.io.din.poke(8.U)
+            dut.clock.step(1)
             dut.io.din.poke(9.U)
+            dut.clock.step(1)
+            dut.io.din.poke(10.U)
+            dut.clock.step(1)
+            dut.io.din.poke(11.U)
+            dut.clock.step(1)
+            dut.io.din.poke(12.U)
+            dut.clock.step(1)
+            dut.io.din.poke(13.U)
+            dut.clock.step(1)
+            dut.io.din.poke(14.U)
+            dut.clock.step(1)
+            dut.io.din.poke(15.U)
+            dut.clock.step(1)
+            dut.io.din.poke(16.U)
+            dut.clock.step(1)
+            dut.io.din.poke(17.U)
+            dut.clock.step(1)
+            dut.io.din.poke(18.U)
+            dut.clock.step(1)
+            dut.io.din.poke(19.U)
+            dut.clock.step(1)
+            dut.io.din.poke(20.U)
+            dut.clock.step(1)
+            dut.io.din.poke(21.U)
+            dut.clock.step(1)
+            dut.io.din.poke(22.U)
+            dut.clock.step(1)
+            dut.io.din.poke(23.U)
+            dut.clock.step(1)
+            dut.io.din.poke(24.U)
+            dut.clock.step(1)
+            dut.io.din.poke(25.U)
+            dut.clock.step(1)
+            dut.io.din.poke(26.U)
+            dut.clock.step(1)
+            dut.io.din.poke(27.U)
+            dut.clock.step(1)
+            dut.io.din.poke(28.U)
+            dut.clock.step(1)
+            dut.io.din.poke(29.U)
+            dut.clock.step(1)
+            dut.io.din.poke(31.U)
+            dut.clock.step(1)
+            dut.io.din.poke(32.U)
+            dut.clock.step(1)
+            dut.io.din.poke(1.U)
             dut.clock.step(1)
             dut.io.din.poke(0.U)
             dut.io.din_v.poke(false.B)
-            
             dut.clock.step(1)
             dut.clock.step(1)
-            dut.clock.step(1)
-            dut.clock.step(1)
-            dut.clock.step(1)
-            dut.clock.step(1)
-            dut.clock.step(1)
-            dut.clock.step(1)
-            dut.clock.step(1)
-            dut.clock.step(1)
-            dut.clock.step(1)
-            dut.clock.step(1)
-
-            
-            
-            /*
-            dut.io.din.poke(1.U)
-            dut.io.din_v.poke(din_v)
-            dut.clock.step(1)
-            dut.io.din.poke(3.U)
-            dut.clock.step(1)
-            dut.io.din.poke(5.U)
-            dut.clock.step(1)
-            dut.io.din.poke(7.U)
-            dut.clock.step(1)
-            dut.io.din.poke(9.U)
-            dut.clock.step(1)
-            dut.io.din.poke(1.U)
-            dut.clock.step(1)
-            dut.io.din.poke(3.U)
-            dut.clock.step(1)
-            dut.io.din.poke(1.U)
-            dut.clock.step(1)
-            dut.io.din.poke(3.U)
-            dut.io.din_v.poke(false.B)
-            
-
-            dut.clock.step(1)
-            dut.clock.step(1)
-            dut.clock.step(1)
-            dut.clock.step(1)
-            dut.clock.step(1)
-            dut.clock.step(1)
-            dut.clock.step(1)
-            dut.clock.step(1)
-            dut.clock.step(1)
-            dut.clock.step(1)
-            dut.clock.step(1)
-            dut.clock.step(1)
-            */
-            /*
-            dut.io.din.poke(din)
-            dut.io.din_v.poke(din_v)
-            dut.io.dout_r.poke(dout_r)
-            println("*************************************")
-            println("*************************************")
-            // Inputs
-            println("Inputs")
-            println("Din: " + dut.io.din.peek().toString)
-            println("Din V: " + dut.io.din_v.peek().toString)
-            println("Dout R: " + dut.io.dout_r.peek().toString)
-            dut.clock.step(1)
-            dut.clock.step(1)
-            dut.clock.step(1)
-            println("--------------------------------------")
-            // Outputs
-            println("Outputs")
-            println("Din R: " + dut.io.din_r.peek().toString)
-            println("Dout: " + dut.io.dout.peek().toString)
-            println("Dout V: " + dut.io.dout_v.peek().toString)
-            println("*************************************")
-            println("*************************************")
-            // Inputs
-            println("Inputs")
-            println("Din: " + dut.io.din.peek().toString)
-            println("Din V: " + dut.io.din_v.peek().toString)
-            println("Dout R: " + dut.io.dout_r.peek().toString)
-            din_v = true.B 
-            dout_r = true.B 
-            // dut.io.din.poke(din)
-            dut.io.din_v.poke(din_v)
-            dut.io.dout_r.poke(dout_r)
-            dut.clock.step(1)
-            dut.clock.step(1)
-            dut.clock.step(1)
-            println("--------------------------------------")
-            // Outputs
-            println("Outputs")
-            println("Din R: " + dut.io.din_r.peek().toString)
-            println("Dout: " + dut.io.dout.peek().toString)
-            println("Dout V: " + dut.io.dout_v.peek().toString)
-            println("*************************************")
-            println("*************************************")
-            din_v = true.B 
-            din = "b101011".U 
-            dout_r = false.B 
-            dut.io.din.poke(din)
-            dut.io.din_v.poke(din_v)
-            dut.io.dout_r.poke(dout_r)
-            println("Din: " + dut.io.din.peek().toString)
-            println("Din V: " + dut.io.din_v.peek().toString)
-            println("Dout R: " + dut.io.dout_r.peek().toString)
-            dut.clock.step(1)
-            dut.clock.step(1)
-            dut.clock.step(1)
-            println("--------------------------------------")
-            // Outputs
-            println("Outputs")
-            println("Din R: " + dut.io.din_r.peek().toString)
-            println("Dout: " + dut.io.dout.peek().toString)
-            println("Dout V: " + dut.io.dout_v.peek().toString)
-            println("*************************************")
-            println("*************************************")
-            din_v = true.B 
-            din = "b101100".U 
-            dout_r = true.B 
-            dut.io.din.poke(din)
-            dut.io.din_v.poke(din_v)
-            dut.io.dout_r.poke(dout_r)
-            // Inputs
-            println("Inputs")
-            println("Din: " + dut.io.din.peek().toString)
-            println("Din V: " + dut.io.din_v.peek().toString)
-            println("Dout R: " + dut.io.dout_r.peek().toString)
-            dut.clock.step(1)
-            dut.clock.step(1)
-            dut.clock.step(1)
-            println("--------------------------------------")
-            // Outputs
-            println("Outputs")
-            println("Din R: " + dut.io.din_r.peek().toString)
-            println("Dout: " + dut.io.dout.peek().toString)
-            println("Dout V: " + dut.io.dout_v.peek().toString)
-            println("*************************************")
-            println("*************************************")
-            din = "b101101".U 
-            dout_r = true.B 
-            dut.io.din.poke(din)
-            dut.io.din_v.poke(din_v)
-            dut.io.dout_r.poke(dout_r)
-            println("Din: " + dut.io.din.peek().toString)
-            println("Din V: " + dut.io.din_v.peek().toString)
-            println("Dout R: " + dut.io.dout_r.peek().toString)
-            dut.clock.step(1)
-            dut.clock.step(1)
-            dut.clock.step(1)
-            println("--------------------------------------")
-            println("Din R: " + dut.io.din_r.peek().toString)
-            println("Dout: " + dut.io.dout.peek().toString)
-            println("Dout V: " + dut.io.dout_v.peek().toString)
-            println("*************************************")
-            println("*************************************")
-            din = "b101110".U 
-            dout_r = false.B 
-            dut.io.din.poke(din)
-            dut.io.din_v.poke(din_v)
-            dut.io.dout_r.poke(dout_r)
-            println("Din: " + dut.io.din.peek().toString)
-            println("Din V: " + dut.io.din_v.peek().toString)
-            println("Dout R: " + dut.io.dout_r.peek().toString)
-            dut.clock.step(1)
-            dut.clock.step(1)
-            dut.clock.step(1)
-            println("--------------------------------------")
-            // Outputs
-            println("Outputs")
-            println("Din R: " + dut.io.din_r.peek().toString)
-            println("Dout: " + dut.io.dout.peek().toString)
-            println("Dout V: " + dut.io.dout_v.peek().toString)
-            println("*************************************")
-            println("*************************************")
-
-
-            /// ====
-            dut.clock.step(1)
-            dut.clock.step(1)
-            dut.clock.step(1)
-            din = "b101110".U 
-            dout_r = true.B 
-            dut.io.din.poke(din)
-            dut.io.din_v.poke(din_v)
-            dut.io.dout_r.poke(dout_r)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 2.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 2.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 2.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 2.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 2.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 2.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 2.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 2.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 2.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 2.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 2.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 2.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 2.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 2.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 2.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 2.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 2.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 2.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 2.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 2.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 2.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 2.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 2.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            din = 1.U 
-            dut.io.din.poke(din)
-            dut.clock.step(1)
-            */
         }
     } 
 }
