c7027f766b98557d9943a08f67bf294bb1ad36c9
yasnakateb <yasnakatebzadeh@gmail.com>
2024-02-06 10:52:18 +0100

    Add vhdl test for D_EB

--- vhdl/D_EB_tb.vhd
+++ vhdl/D_EB_tb.vhd
@@ -2,53 +2,45 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity overlay_Rocc_tb is
-end overlay_Rocc_tb;
+entity D_EB_tb is
+end D_EB_tb;
 
-architecture testbench of overlay_Rocc_tb is
+architecture testbench of D_EB_tb is
 
     constant C_RST_POL         : std_logic := '1';
-    constant C_FIXED_POINT     : boolean   := false;
-    constant C_FRACTION_LENGTH : integer   := 16;
     constant C_DATA_WIDTH      : integer   := 32; 
-    constant C_INPUT_NODES     : integer   := 6; 
-    constant C_OUTPUT_NODES    : integer   := 6;
-    constant C_FIFO_DEPTH      : integer   := 32;
     
-    signal clk             : std_logic;
-    signal reset           : std_logic;
-    signal data_in         : std_logic_vector(C_DATA_WIDTH*C_INPUT_NODES - 1 downto 0);
-    signal data_in_valid   : std_logic_vector(C_INPUT_NODES - 1 downto 0);
-    signal data_in_ready   : std_logic_vector(C_INPUT_NODES - 1 downto 0);
-    signal data_out        : std_logic_vector(C_DATA_WIDTH*C_INPUT_NODES - 1 downto 0);
-    signal data_out_valid  : std_logic_vector(C_INPUT_NODES - 1 downto 0);
-    signal data_out_ready  : std_logic_vector(C_INPUT_NODES - 1 downto 0);
-    signal cell_config     : std_logic_vector(191 downto 0);
+    signal clk      : std_logic;
+    signal reset    : std_logic;
+
+    signal  din     : std_logic_vector(C_DATA_WIDTH-1 downto 0);
+    signal  din_v   : std_logic;
+    signal  din_r   : std_logic;
+        
+    signal  dout    : std_logic_vector(C_DATA_WIDTH-1 downto 0);
+    signal  dout_v  : std_logic;
+    signal  dout_r  : std_logic;
     
     constant clk_period : time := 10 ns;
 
 begin
 
-    dut: entity work.overlay_rocc
+    dut: entity work.D_EB 
     generic map (
-        C_RST_POL         => C_RST_POL        ,
-        C_FIXED_POINT     => C_FIXED_POINT    ,
-        C_FRACTION_LENGTH => C_FRACTION_LENGTH,
-        C_DATA_WIDTH      => C_DATA_WIDTH     ,
-        C_INPUT_NODES     => C_INPUT_NODES    ,
-        C_OUTPUT_NODES    => C_OUTPUT_NODES   ,
-        C_FIFO_DEPTH      => C_FIFO_DEPTH
+        C_RST_POL     => C_RST_POL ,
+        C_DATA_WIDTH  => C_DATA_WIDTH     
+    
     )
     port map ( 
-        clk             => clk           ,
-        reset           => reset         ,
-        data_in         => data_in       ,
-        data_in_valid   => data_in_valid ,
-        data_in_ready   => data_in_ready ,
-        data_out        => data_out      ,
-        data_out_valid  => data_out_valid,
-        data_out_ready  => data_out_ready,
-        cell_config     => cell_config
+        clk     => clk   ,
+        reset   => reset ,
+        din     => din   ,    
+        din_v   => din_v ,
+        din_r   => din_r ,
+        dout    => dout  ,
+        dout_v  => dout_v,
+        dout_r  => dout_r
+        
     );
 
     process
@@ -61,33 +53,25 @@ begin
     
     process
     begin
-        reset          <= C_RST_POL;
-        data_in        <= (others => '0');
-        data_in_valid  <= (others => '0');
-        data_out_ready <= (others => '1');
+        reset <= C_RST_POL;
+        din   <= (others => '0');
+        din_v <= '0';
+        dout_r <= '1';
         
-        cell_config    <= (others => '0');
         wait for clk_period * 100.5;
         
         reset <= '0';
         wait for clk_period * 10;
         
-        -- Config
-        cell_config <= "100010000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000100000000000000000100000000";
-        wait for clk_period;
-        cell_config <= "100010100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001000000000010000000000000001000000000000000000000000011";
-        wait for clk_period;
-        cell_config <= (others => '0');
-        wait for clk_period * 10;
-        
+    
         -- Operate
         for i in 0 to 15 loop
         
-            data_in(C_DATA_WIDTH*C_INPUT_NODES-1 downto C_DATA_WIDTH*(C_INPUT_NODES-1)) <= std_logic_vector(to_unsigned(i, C_DATA_WIDTH));
-            data_in(C_DATA_WIDTH*(C_INPUT_NODES-1)-1 downto C_DATA_WIDTH*(C_INPUT_NODES-2)) <= std_logic_vector(to_unsigned(2*i, C_DATA_WIDTH));
-            data_in_valid <= "110000";
+            din(C_DATA_WIDTH -1 downto 0) <= std_logic_vector(to_unsigned(i, C_DATA_WIDTH));
+            
+            din_v<= '1';
             wait for (i + 1) * clk_period;
-            data_in_valid <= "000000";
+            din_v <= '0';
             wait for clk_period * 10;
             
         end loop;
