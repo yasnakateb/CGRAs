c02bddf88d9d0b8ed5d27505614a8523a15dcc27
yasnakateb <yasnakatebzadeh@gmail.com>
2024-02-06 15:12:56 +0100

    Add vhdl test for CellProcessing

--- vhdl/CellProcessing_tb.vhd
+++ vhdl/CellProcessing_tb.vhd
@@ -2,53 +2,67 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity overlay_Rocc_tb is
-end overlay_Rocc_tb;
+entity CellProcessing_tb is
+end CellProcessing_tb;
 
-architecture testbench of overlay_Rocc_tb is
+architecture testbench of CellProcessing_tb is
 
     constant C_RST_POL         : std_logic := '1';
+    constant C_DATA_WIDTH      : integer   := 32; 
     constant C_FIXED_POINT     : boolean   := false;
     constant C_FRACTION_LENGTH : integer   := 16;
-    constant C_DATA_WIDTH      : integer   := 32; 
-    constant C_INPUT_NODES     : integer   := 6; 
-    constant C_OUTPUT_NODES    : integer   := 6;
-    constant C_FIFO_DEPTH      : integer   := 32;
     
-    signal clk             : std_logic;
-    signal reset           : std_logic;
-    signal data_in         : std_logic_vector(C_DATA_WIDTH*C_INPUT_NODES - 1 downto 0);
-    signal data_in_valid   : std_logic_vector(C_INPUT_NODES - 1 downto 0);
-    signal data_in_ready   : std_logic_vector(C_INPUT_NODES - 1 downto 0);
-    signal data_out        : std_logic_vector(C_DATA_WIDTH*C_INPUT_NODES - 1 downto 0);
-    signal data_out_valid  : std_logic_vector(C_INPUT_NODES - 1 downto 0);
-    signal data_out_ready  : std_logic_vector(C_INPUT_NODES - 1 downto 0);
-    signal cell_config     : std_logic_vector(191 downto 0);
+    signal clk          : std_logic;
+    signal reset        : std_logic;
+    signal north_din    : std_logic_vector (C_DATA_WIDTH-1 downto 0);
+    signal north_din_v  : std_logic;
+    signal east_din     : std_logic_vector (C_DATA_WIDTH-1 downto 0);
+    signal east_din_v   : std_logic;
+    signal south_din    : std_logic_vector (C_DATA_WIDTH-1 downto 0);
+    signal south_din_v  : std_logic;
+    signal west_din     : std_logic_vector (C_DATA_WIDTH-1 downto 0);
+    signal west_din_v   : std_logic;
+    signal FU_din_1_r   : std_logic;
+    signal FU_din_2_r   : std_logic;
+    signal dout         : std_logic_vector (C_DATA_WIDTH-1 downto 0);
+    signal dout_v       : std_logic;
+    signal north_dout_r : std_logic;
+    signal east_dout_r  : std_logic;
+    signal south_dout_r : std_logic;
+    signal west_dout_r  : std_logic;
+    signal config_bits  : std_logic_vector (181 downto 0);
     
     constant clk_period : time := 10 ns;
 
 begin
 
-    dut: entity work.overlay_rocc
+    dut: entity work.cell_processing
     generic map (
         C_RST_POL         => C_RST_POL        ,
-        C_FIXED_POINT     => C_FIXED_POINT    ,
-        C_FRACTION_LENGTH => C_FRACTION_LENGTH,
         C_DATA_WIDTH      => C_DATA_WIDTH     ,
-        C_INPUT_NODES     => C_INPUT_NODES    ,
-        C_OUTPUT_NODES    => C_OUTPUT_NODES   ,
-        C_FIFO_DEPTH      => C_FIFO_DEPTH
+        C_FIXED_POINT     => C_FIXED_POINT    ,
+        C_FRACTION_LENGTH => C_FRACTION_LENGTH      
     )
-    port map ( 
-        clk             => clk           ,
-        reset           => reset         ,
-        data_in         => data_in       ,
-        data_in_valid   => data_in_valid ,
-        data_in_ready   => data_in_ready ,
-        data_out        => data_out      ,
-        data_out_valid  => data_out_valid,
-        data_out_ready  => data_out_ready,
-        cell_config     => cell_config
+    port map (
+        clk          => clk,
+        reset        => reset,
+        north_din    => north_din,
+        north_din_v  => north_din_v,
+        east_din     => east_din,
+        east_din_v   => east_din_v,
+        south_din    => south_din,
+        south_din_v  => south_din_v,
+        west_din     => west_din,
+        west_din_v   => west_din_v,
+        FU_din_1_r   => FU_din_1_r,
+        FU_din_2_r   => FU_din_2_r,
+        dout         => dout,
+        dout_v       => dout_v,
+        north_dout_r => north_dout_r,
+        east_dout_r  => east_dout_r,
+        south_dout_r => south_dout_r,
+        west_dout_r  => west_dout_r,
+        config_bits  => config_bits
     );
 
     process
@@ -61,36 +75,42 @@ begin
     
     process
     begin
-        reset          <= C_RST_POL;
-        data_in        <= (others => '0');
-        data_in_valid  <= (others => '0');
-        data_out_ready <= (others => '1');
-        
-        cell_config    <= (others => '0');
+        north_din <= (others => '0');
+        north_din_v <= '0';
+        east_din <= (others => '0');
+        east_din_v <= '0';
+        south_din <= (others => '0');
+        south_din_v <= '0';
+        west_din <= (others => '0');
+        west_din_v <= '0';
+        north_dout_r <= '0';
+        east_dout_r <= '0';
+        south_dout_r <= '0';
+        west_dout_r <= '0';
+        config_bits <= (others => '0');
+
+        reset <= '1';
         wait for clk_period * 100.5;
         
         reset <= '0';
         wait for clk_period * 10;
         
         -- Config
-        cell_config <= "100010000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000100000000000000000100000000";
-        wait for clk_period;
-        cell_config <= "100010100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001000000000010000000000000001000000000000000000000000011";
-        wait for clk_period;
-        cell_config <= (others => '0');
+        config_bits <= "00000000000111101100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001000000000010000000000000001000000000000000000000000011";
+      
         wait for clk_period * 10;
         
         -- Operate
-        for i in 0 to 15 loop
-        
-            data_in(C_DATA_WIDTH*C_INPUT_NODES-1 downto C_DATA_WIDTH*(C_INPUT_NODES-1)) <= std_logic_vector(to_unsigned(i, C_DATA_WIDTH));
-            data_in(C_DATA_WIDTH*(C_INPUT_NODES-1)-1 downto C_DATA_WIDTH*(C_INPUT_NODES-2)) <= std_logic_vector(to_unsigned(2*i, C_DATA_WIDTH));
-            data_in_valid <= "110000";
-            wait for (i + 1) * clk_period;
-            data_in_valid <= "000000";
-            wait for clk_period * 10;
-            
-        end loop;
+        north_din(C_DATA_WIDTH-1 downto 0) <= std_logic_vector(to_unsigned(2, C_DATA_WIDTH));
+        west_din(C_DATA_WIDTH-1 downto 0) <= std_logic_vector(to_unsigned(4, C_DATA_WIDTH));
+        north_din_v <= '1';
+        west_din_v <= '1'; 
+        wait for (5) * clk_period;
+        north_din_v <= '0';
+        west_din_v <= '0'; 
+        north_din <= (others => '0');
+        west_din <= (others => '0');
+        wait for clk_period * 100;
         
         assert false report "End of Simulation!" severity failure;
         wait;
